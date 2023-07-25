4745d00e7cbf7d579f540abc6eb77b3db83a2dde
userfiona <fionaramaesela@gmail.com>
2023-07-25 08:43:10 -0700

    operation

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -86,5 +86,4 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	lomuto_sort(array, size, 0, size - 1);
-}                                                
+	lomuto_sort(array, size, 0, size - 1);}                                                
